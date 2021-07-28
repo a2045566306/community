dc2ac444169d7b4ffc07bd918596a6741b27f97c
margictour <a2045566306@gmail.com>
2021-07-28 23:35:26 +0800

    实现分页功能

--- communtiy/src/main/java/life/majiang/communtiy/communtiy/controller/IndexController.java
+++ communtiy/src/main/java/life/majiang/communtiy/communtiy/controller/IndexController.java
@@ -1,5 +1,6 @@
 package life.majiang.communtiy.communtiy.controller;
 
+import life.majiang.communtiy.communtiy.dto.PaginationDTO;
 import life.majiang.communtiy.communtiy.dto.QuestionDTO;
 import life.majiang.communtiy.communtiy.mapper.QuestionMapper;
 import life.majiang.communtiy.communtiy.mapper.UserMapper;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -28,7 +30,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "2") Integer size) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -43,8 +47,8 @@ public class IndexController {
             }
 
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- communtiy/src/main/java/life/majiang/communtiy/communtiy/service/QuestionService.java
+++ communtiy/src/main/java/life/majiang/communtiy/communtiy/service/QuestionService.java
@@ -1,5 +1,6 @@
 package life.majiang.communtiy.communtiy.service;
 
+import life.majiang.communtiy.communtiy.dto.PaginationDTO;
 import life.majiang.communtiy.communtiy.dto.QuestionDTO;
 import life.majiang.communtiy.communtiy.mapper.QuestionMapper;
 import life.majiang.communtiy.communtiy.mapper.UserMapper;
@@ -21,9 +22,26 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+
+
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+
+        if (page < 1) {
+            page = 1;
+        }
+
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+        //size*(page - 1)
+
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for (Question question : questions) {
             User user = userMapper.findByID(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -31,6 +49,9 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
     }
 }
