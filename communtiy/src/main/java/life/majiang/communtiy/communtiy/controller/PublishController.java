3b18070bb480c81bfc769e06c7958ae6ccb387e0
margictour <a2045566306@gmail.com>
2021-07-29 22:38:30 +0800

    发布问题列表实现+拦截器

--- communtiy/src/main/java/life/majiang/communtiy/communtiy/controller/PublishController.java
+++ communtiy/src/main/java/life/majiang/communtiy/communtiy/controller/PublishController.java
@@ -1,7 +1,6 @@
 package life.majiang.communtiy.communtiy.controller;
 
 import life.majiang.communtiy.communtiy.mapper.QuestionMapper;
-import life.majiang.communtiy.communtiy.mapper.UserMapper;
 import life.majiang.communtiy.communtiy.model.Question;
 import life.majiang.communtiy.communtiy.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -19,9 +16,6 @@ public class PublishController {
 
     @Autowired
     private QuestionMapper questionMapper;
-
-    @Autowired
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish() {
@@ -53,19 +47,7 @@ public class PublishController {
             return "publish";
         }
 
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
+        User user = (User) request.getSession().getAttribute("user");
 
         if (user == null) {
             model.addAttribute("error", "用户未登录");
@@ -80,7 +62,7 @@ public class PublishController {
             question.setGmtCreate(System.currentTimeMillis());
             question.setGmtModified(question.getGmtModified());
             questionMapper.craete(question);
-            return "redirect:/";
+            return " :/";
         }
 }
 
--- communtiy/src/main/java/life/majiang/communtiy/communtiy/service/QuestionService.java
+++ communtiy/src/main/java/life/majiang/communtiy/communtiy/service/QuestionService.java
@@ -23,8 +23,6 @@ public class QuestionService {
     private UserMapper userMapper;
 
     public PaginationDTO list(Integer page, Integer size) {
-
-
         PaginationDTO paginationDTO = new PaginationDTO();
         Integer totalCount = questionMapper.count();
         paginationDTO.setPagination(totalCount,page,size);
@@ -40,6 +38,37 @@ public class QuestionService {
 
         Integer offset = size * (page - 1);
         List<Question> questions = questionMapper.list(offset,size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        for (Question question : questions) {
+            User user = userMapper.findByID(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
+    }
+
+    public PaginationDTO list(Integer userid, Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.countByUserId(userid);
+        paginationDTO.setPagination(totalCount,page,size);
+
+        if (page < 1) {
+            page = 1;
+        }
+
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+
+        //size*(page - 1)
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.listByUserId(userid,offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
         for (Question question : questions) {
